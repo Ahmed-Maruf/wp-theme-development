2915659fd49cbcb1ef9a637122ca15189f596240
Ahmed Maruf <maruf.ahmed@sjinnovation.com>
2019-07-19 18:11:42 +0600

    luncher countdown added

--- luncher/assets/js/main.js
+++ luncher/assets/js/main.js
@@ -1,103 +1,103 @@
-;(function ($) {
-	
+; (function ($) {
+
 	'use strict';
 
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+		$('.animate-box').waypoint(function (direction) {
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .animate-box.item-animate').each(function(k){
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight animated-fast');
 							} else {
 								el.addClass('fadeInUp animated-fast');
 							}
 
 							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
+						}, k * 100, 'easeInOutExpo');
 					});
-					
+
 				}, 100);
-				
+
 			}
 
-		} , { offset: '85%' } );
+		}, { offset: '85%' });
 	};
 
 
 
 	// Loading page
-	var loaderPage = function() {
+	var loaderPage = function () {
 		$(".fh5co-loader").fadeOut("slow");
 	};
 
 
-	var screenHeight = function() {
+	var screenHeight = function () {
 
-		if ( $(window).width() > 768 && !isMobile.any() ) {
+		if ($(window).width() > 768 && !isMobile.any()) {
 			$('.js-dt, .js-dtc').css('min-height', $(window).height());
 		} else {
 			$('.js-dt, .js-dtc').css('min-height', '');
 		}
-		$(window).resize(function(){
-			if ( $(window).width() > 768 && !isMobile.any() ) {
+		$(window).resize(function () {
+			if ($(window).width() > 768 && !isMobile.any()) {
 				$('.js-dt, .js-dtc').css('min-height', $(window).height());
 			} else {
 				$('.js-dt, .js-dtc').css('min-height', '');
 			}
 		});
-		
+
 	};
 
-	var countDown = function() {
+	var countDown = function () {
 
-		simplyCountdown('.simply-countdown-one', {
-			year: "2018",
-			month: "05",
-			day: "30"
+		// jQuery Example
+		$('.simply-countdown-one').simplyCountdown({
+			year: 2019, // required
+			month: 12, // required
+			day: 28 // required
 		});
 
 	};
-	
-	
-	
-	$(function(){
+
+
+
+	$(function () {
 		contentWayPoint();
 		loaderPage();
 		screenHeight();
